7a48460694cce3f2bd9829f0b25b03c001764eb8
Christer Edvartsen <cogo@starzinger.net>
2017-01-11 17:17:11 +0100

    Add custom exception class for assertion failures

---
+++ src/Exception/AssertionFailedException.php
@@ -0,0 +1,11 @@
+<?php
+namespace Imbo\BehatApiExtension\Exception;
+
+use Exception;
+
+/**
+ * Assertion failure exception
+ *
+ * @author Christer Edvartsen <cogo@starzinger.net>
+ */
+class AssertionFailedException extends Exception {}

d08e9a6b59ad762ac76a5cfeef89e5df6155fd8d
John Zwarthoed <john.zwarthoed@vancis.nl>
2017-10-20 09:58:45 -0400

    Add Oauth 2 JWT token support

--- tests/Context/ApiContextTest.php
+++ tests/Context/ApiContextTest.php
@@ -1007,6 +1007,21 @@ BAR;
     }
 
     /**
+     * @covers ::assertJsonFieldContainsValidJWT
+     * @group assertions
+     */
+    public function testCanAssertThatTheResponseFieldContainsAJWT() {
+        $this->mockHandler->append(new Response(200, [], json_encode(['access_token' => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InRlc3QifQ.eyJzdWIiOiJmb29AYmFyLmNvbSIsImlzcyI6InNvbWUgaXNzdWVyIn0.ZlHxUiLh3E133S5aE0F5OhVZjbyQJftG-WQnrRpxnMo"])));
+        $this->comparator
+            ->expects($this->once())
+            ->method('compare')
+            ->with(['sub' => 'foo@bar.com', 'iss' => 'some issuer'], ['sub' => 'foo@bar.com', 'iss' => 'some issuer'])
+            ->will($this->returnValue(true));
+        $this->context->requestPath('/some/path');
+        $this->context->assertJsonFieldContainsValidJWT('access_token', new PyStringNode(['{"header":{"alg":"HS256","typ":"JWT","kid":"test"},"claims":{"sub":"foo@bar.com","iss":"some issuer"},"secret":"secret"}'], 1));
+    }
+
+    /**
      * @dataProvider getResponseBodyArrays
      * @covers ::assertResponseBodyJsonArrayLength
      * @covers ::getResponseBodyArray

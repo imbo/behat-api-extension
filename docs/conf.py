b77414686ba297da9172efdeb8ef39efabe5026f
Christer Edvartsen <cogo@starzinger.net>
2025-01-07 12:38:15 +0100

    Try to set a different theme for the docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.phpdomain']
+extensions = ['sphinxcontrib.phpdomain', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -100,7 +100,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
